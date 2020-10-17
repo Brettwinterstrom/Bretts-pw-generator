41e17bb9e455e578d7cd51f4d178ac765a464089
Brett Winterstrom <brettwinterstrom@yahoo.com>
2020-10-16 19:07:27 -0700

    Completed all of the functionality of the generator

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
 // Variables
-var generateBtn = document.querySelector("#generate");
-let lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-let upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-let numbers = ["1","2","3","4","5","6","7","8","9"]
-let specialChar = ["!","@","#","$","%","^","&","*","-","_","=","+"]
-let newPassword = ''
-var chosenCharacters = []
+const generateBtn = document.querySelector("#generate");
+const lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "+"];
 
-// Asks user how many characters they want in thier password
-const charLength = window.prompt('How many characters would you like in your password?');
-// Asks user if they would like special characters in their password
-const doSpecial = window.confirm('Would you like to include special characters in your password?');
-// Asks user if they would like numeric characters in their password
-const doNumber = window.confirm('Would you like to include numeric characters in you password?');
-// Asks user if they would like lowercase characters in their password
-const doLowers = window.confirm('Would you like to include lowercase characters in you password?');
-// Asks user if they would like uppercase characters in their password
-const doUppers = window.confirm('Would you like to include uppercase characters in you password?');
+function generatePassword() {
+  const newPassword = [];
+  let chosenCharacters = [];
 
-// Takes User Input and if input === true then it adds that specific array to a new array
-if (doSpecial === true) {
-  chosenCharacters.concat(specialChar);
-} 
-if (doNumber === true) {
-  chosenCharacters.concat(numbers);
+  // Asks user how many characters they want in thier password
+  const charLength = parseInt(prompt('How many characters would you like in your password?'));
+
+  while (charLength === NaN) {
+    alert("Hey what you inputed is not a number");
+    charLength = parseInt(prompt('How many characters would you like in your password?'));
+  }
+
+  while (charLength < 8 || charLength > 128) {
+    alert("Hey you are out of range");
+    charLength = parseInt(prompt('How many characters would you like in your password?'));
+  }
+
+  // Asks user if they would like special characters in their password
+  const doSpecial = confirm('Would you like to include special characters in your password?');
+  // Asks user if they would like numeric characters in their password
+  const doNumber = confirm('Would you like to include numeric characters in you password?');
+  // Asks user if they would like lowercase characters in their password
+  const doLowers = confirm('Would you like to include lowercase characters in you password?');
+  // Asks user if they would like uppercase characters in their password
+  const doUppers = confirm('Would you like to include uppercase characters in you password?');
+
+  // Takes User Input and if input === true then it adds that specific array to a new array
+  if (doSpecial) {
+    chosenCharacters = chosenCharacters.concat(specialChar);
+  }
+  if (doNumber) {
+    chosenCharacters = chosenCharacters.concat(numbers);
+  }
+  if (doLowers) {
+    chosenCharacters = chosenCharacters.concat(lowerLetters);
+  }
+  if (doUppers) {
+    chosenCharacters = chosenCharacters.concat(upperLetters);
+  }
+
+  console.log(chosenCharacters);
+
+  //Loops through New array with user chosen characters and picks 10 elements at random
+  for (i = 0; i < charLength; i++) {
+    var randomChar = chosenCharacters[Math.floor(Math.random() * chosenCharacters.length)];
+    newPassword.push(randomChar);
+  };
+
+  //update the textarea with new password
+  document.querySelector("#password").value = newPassword.join("");
 }
-if (doLowers === true) {
-  chosenCharacters.concat(lowerLetters);
-}
-if (doUppers === true) {
-  chosenCharacters.concat(upperLetters);
-}
-
-console.log(chosenCharacters);
-
-//Loops through New array with user chosen characters and picks 10 elements at random
-for(i = 0; i = length/* or i = charLength*/; i++){
-var char = charAt(Math.floor(Math.random() * chosenCharacters.length));
-newPassword = newPassword + char;
-};
-
 
 
 // Write password to the #password input
-function writePassword() {
+// function writePassword() {
 
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword )
+generateBtn.addEventListener("click", generatePassword);
   // declare settings from DOM
-     
 
-      
+
+
 
